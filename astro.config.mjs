9f7cb18ea87e158e887643223252a95d7d76d989
rabbits <147097423+rabbits0209@users.noreply.github.com>
2025-08-15 15:31:42 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://www.xn--eet944d.top/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
