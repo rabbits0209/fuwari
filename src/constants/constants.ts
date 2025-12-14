4ea5821f4dc181a32b145ffe1636f609093e0040
tutuxka <147097423+rabbits0209@users.noreply.github.com>
2025-12-14 13:00:18 +0800

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,7 +6,8 @@ export const LIGHT_MODE = "light",
 export const DEFAULT_THEME = AUTO_MODE;
 
 // Banner height unit: vh
-export const BANNER_HEIGHT = 35;
+// Make the hero/banner area taller so the first screen has a large visual header
+export const BANNER_HEIGHT = 55;
 export const BANNER_HEIGHT_EXTEND = 30;
 export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
 
